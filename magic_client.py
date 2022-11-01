1e3e31292116120dfcacf433a60ef1ea0496c8d5
Joshua Randle <randlejo@oregonstate.edu>
2022-10-31 21:36:16 -0500

    communication contract

--- magic_client.py
+++ magic_client.py
@@ -5,28 +5,19 @@ import zmq
 def magic_rng_card():
 
     context = zmq.Context()
-    start = input("Input 'start' to get a random card name or 'end' to leave rng ")
-
-    if start == 'start':
-
+    
 #  Socket to talk to server
-        print("Connecting to rng name server…")
-        socket = context.socket(zmq.REQ)
-        socket.connect("tcp://localhost:5555")
+    print("Connecting to rng name server…")
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
 
-        print("Sending request…")
-        socket.send_string("Generating random card name...")
+    print("Sending request…")
+    socket.send_string("Generating random card name...")
 
         #  Get the reply.
-        message = socket.recv()
-        card_name = message.decode('utf-8')
-        return(card_name)
-        
-    elif start == 'end':
-        print ("Ok thanks for playing!")
+    message = socket.recv()
+    return message.decode('utf-8')
+    
 
-    else:
-        print("Sorry you have entered the wrong input, try again")
-        magic_rng_card()
-
-magic_rng_card()
+card_name = magic_rng_card() 
+print(card_name)
